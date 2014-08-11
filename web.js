c5293961fa55dc43b0a65739c60ff814b3604001
Scott Doxey <neogeek0101@gmail.com>
2014-08-10 20:10:40 -0400

    Added caching support through redid.

--- web.js
+++ web.js
@@ -4,30 +4,52 @@ var doxdox = require('doxdox');
 var express = require('express');
 var server = express();
 
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, { no_ready_check: true });
+client.auth(redisURL.auth.split(':')[1]);
+
 var rawgit_url = 'https://raw.githubusercontent.com/';
 
 server.use(express.static(__dirname + '/static'));
 
 server.get('/:username/:repo', function (req, res) {
 
-    request.get({
-        url: rawgit_url + req.params.username + '/' + req.params.repo + '/master/package.json',
-        json: true
-    }, function (e, r, body) {
+    client.get(req.url, function (err, reply) {
 
-        var config = {
-            title: body.name,
-            description: body.description,
-            layout: 'bootstrap'
-        };
+        if (reply) {
 
-        request.get({
-            url: rawgit_url + req.params.username + '/' + req.params.repo + '/master/' + body.main
-        }, function (e, r, body) {
+            res.send(reply);
 
-            res.send(doxdox.parseScript(body, null, config));
+        } else {
 
-        });
+            request.get({
+                url: rawgit_url + req.params.username + '/' + req.params.repo + '/master/package.json',
+                json: true
+            }, function (e, r, body) {
+
+                var config = {
+                    title: body.name,
+                    description: body.description,
+                    layout: 'bootstrap'
+                };
+
+                request.get({
+                    url: rawgit_url + req.params.username + '/' + req.params.repo + '/master/' + body.main
+                }, function (e, r, body) {
+
+                    body = doxdox.parseScript(body, null, config);
+
+                    client.set(req.url, body);
+
+                    res.send(body);
+
+                });
+
+            });
+
+        }
 
     });
 
